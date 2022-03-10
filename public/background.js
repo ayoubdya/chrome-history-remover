fc34a3b21032d436011b2d9a23782472c98634fb
Ayoub <ayoubdya@gmail.com>
2022-03-10 11:09:01 +0100

    minor change

--- public/background.js
+++ public/background.js
@@ -15,7 +15,7 @@ const deleteHistory = (url) =>
 chrome.history.onVisited.addListener((result) => {
   console.log(result);
   const host = result.url.match(
-    /https?:\/\/(([a-zA-Z0-9()]+\.)?([-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}))\b[-a-zA-Z0-9()@:%_\+.~#?&//=]*/
+    /https?:\/\/(([a-zA-Z0-9]+\.)?([-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}))\b[-a-zA-Z0-9()@:%_\+.~#?&//=]*/
   )[1];
   chrome.storage.sync.get("blockedUrls").then(({ blockedUrls }) => {
     if (blockedUrls !== undefined && blockedUrls.includes(host)) {
